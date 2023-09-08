9efbf6027ac81cfd3374d75414ec84be3bf38ec9
Debrath Sharma <debrath.sharma@hazesoft.co>
2023-09-08 16:57:25 +0545

    init: project initialized

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Simple message method
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function message(Request $request): array
+    {
+        return [];
+    }
+}
